e0836dd77149aed2d8795b5ddb4948faa1709ff7
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2020-01-19 12:36:09 +0100

    View coffee with recipe

--- routes/coffee.js
+++ routes/coffee.js
@@ -5,24 +5,92 @@ const db = require('../db');
 
 const router = new Router();
 
+/**
+ *  Przeznaczenie: Zwraca wszystkie kawy wraz z przepisami
+ *  Metoda: GET
+ *  URL: 'coffee/'
+ *
+ *  Struktura odpowiedzi:
+ *  [
+ *    {
+ *      id: Integer
+ *      name: String
+ *      productionCost: Integer
+ *      priceMargin: Float
+ *      status: 'niedostepny' | 'niska_dostepnosc' | 'dostepny'
+ *      recipe: [
+ *        {
+ *          coffeeTypeId: Integer
+ *          ingredientId: Integer
+ *          name: String
+ *          amount: Integer
+ *          unit: String
+ *        },
+ *        { ... }
+ *      ]
+ *    },
+ *    { ... }
+ *  ]
+ *
+ *  Przykładowa odpowiedź:
+ *  [
+ *    {
+ *      "id": 2,
+ *      "name": "cappucino",
+ *      "productionCost": "5.29",
+ *      "priceMargin": "2.50",
+ *      "status": "dostepny",
+ *      "recipe": [
+ *        {
+ *          "coffeeTypeId": 2,
+ *          "ingredientId": 2,
+ *          "name": "kimbo aroma gold",
+ *          "amount": 0.07,
+ *          "unit": "kilogram"
+ *        },
+ *        { ... }
+ *      ]
+ *    },
+ *    { ... }
+ *  ]
+ * */
 router.get('/', async (req, res) => {
-  const { rows } = await db.query(sql`
+  const { rows: coffeeType } = await db.query(sql`
     SELECT * FROM rodzaje_kawy
   `);
 
-  const mappedCoffeeType = rows.map(coffee => ({
+  const mappedCoffeeType = coffeeType.map(coffee => ({
     id: coffee.id,
     name: coffee.nazwa,
-    productionCost: coffee.cena_skladnikow,
-    priceMargin: coffee.marza,
-    status: coffee.status
+    productionCost: parseFloat(coffee.cena_skladnikow),
+    priceMargin: parseFloat(coffee.marza),
+    status: coffee.status,
+    recipe: []
   }));
 
-  const returnData = {
-    //TODO
-  };
+  const { rows: recipes } = await db.query(sql`
+    SELECT rodzaj_kawy_id, skladnik_id, p.ilosc, nazwa, jednostka
+    FROM przepisy p
+    JOIN skladniki s
+    ON p.skladnik_id = s.id
+  `);
 
-  return res.status(200).send();
+  const mappedRecipes = recipes.map(recipe => ({
+    coffeeTypeId: recipe.rodzaj_kawy_id,
+    ingredientId: recipe.skladnik_id,
+    name: recipe.nazwa,
+    amount: parseFloat(recipe.ilosc),
+    unit: recipe.jednostka
+  }));
+
+  mappedCoffeeType.forEach(coffee => {
+    mappedRecipes.forEach(item => {
+      if (item.coffeeTypeId === coffee.id)
+        coffee.recipe.push(item)
+    })
+  });
+
+  return res.status(200).send(mappedCoffeeType);
 });
 
 /**
@@ -68,7 +136,7 @@ router.post('/', async (req, res) => {
     VALUES (${name}, ${priceMargin})
     RETURNING id
   `);
-  const {id: coffeeId} = rows[0];
+  const { id: coffeeId } = rows[0];
 
   const promises = recipe.map(item => (
     db.query(sql`
